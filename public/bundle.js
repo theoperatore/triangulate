50e60d3e9c97f55bf2d20c4000d770703fed0359
Alex Petersen <theoperatore@gmail.com>
2014-08-25 16:56:37 -0500

    initial pages commit

--- public/bundle.js
+++ public/bundle.js
@@ -1,3 +1,4 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var cvs = document.createElement('div'),
     map,
     opts = {};
@@ -29,7 +30,7 @@ if (navigator && navigator.geolocation) {
     },
     function(error) {
       console.log(error);
-      cvs.innerHTML = "Error obtaining GPS location from network: " + error.message;
+      cvs.innerHTML = "Error obtaining GPS location from network: " + error;
     }, {enableHighAccuracy: true});
 }
 else {
@@ -72,3 +73,4 @@ function convertToDMS(coords) {
 
   return out;
 }
+},{}]},{},[1]);
