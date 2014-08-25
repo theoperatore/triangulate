10ca7884d82c10dd979538b7faf1a56f52cbea00
Alex Petersen <theoperatore@gmail.com>
2014-08-25 17:12:51 -0500

    map size

--- public/index.js
+++ public/index.js
@@ -3,11 +3,11 @@ var cvs = document.createElement('div'),
     opts = {};
 
 cvs.style.width = window.innerWidth + "px";
-cvs.style.height = window.innerHeight - 100 + "px";
+cvs.style.height = window.innerHeight - 25 + "px";
 document.body.appendChild(cvs);
 document.addEventListener('resize', function() {
   cvs.style.width = window.innerWidth + "px";
-  cvs.style.height = window.innerHeight - 100 + "px";
+  cvs.style.height = window.innerHeight - 25 + "px";
 }, false);
 
 cvs.innerHTML = "fetching location...";
