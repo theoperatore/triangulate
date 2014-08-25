6d4d367c14809ebe7357b4c1e2e299e950faf0c2
Alex Petersen <theoperatore@gmail.com>
2014-08-25 16:52:25 -0500

    google maps centering/position output

--- public/index.js
+++ public/index.js
@@ -1,30 +1,74 @@
-var cvs = document.createElement('canvas'),
-    ctx = cvs.getContext('2d'),
+var cvs = document.createElement('div'),
     map,
     opts = {};
 
-cvs = require('hd-canvas')(cvs, window.innerWidth, window.innerHeight * (3/4));
+cvs.style.width = window.innerWidth + "px";
+cvs.style.height = window.innerHeight * (3/4) + "px";
 document.body.appendChild(cvs);
 document.addEventListener('resize', function() {
-  cvs= require('hd-canvas')(cvs, window.innerWidth, window.innerHeight * (3/4));
+  cvs.style.width = window.innerWidth + "px";
+  cvs.style.height = window.innerHeight * (3/4) + "px";
 }, false);
 
-// initial text
-ctx.beginPath();
-ctx.font = "100 24px 'Helvetica Neue', 'Helvetica' ";
-ctx.fillText("fetching location...", 0,
-  (parseInt(cvs.style.height) ? parseInt(cvs.style.height) / 2 : cvs.height/2)
-);
+cvs.innerHTML = "fetching location...";
 
 if (navigator && navigator.geolocation) {
   var pos = navigator.geolocation.getCurrentPosition(
     // success
     function(loc) {
       console.log(loc.coords.latitude, loc.coords.longitude, loc.coords.accuracy, loc);
+      opts.mapTypeId = google.maps.MapTypeId.HYBRID;
+      opts.zoom = 18;
+      opts.center = new google.maps.LatLng(loc.coords.latitude, loc.coords.longitude);
+      map = new google.maps.Map(cvs, opts);
+
+      var locOut = document.createElement('p');
+      locOut.innerHTML = convertToDMS(loc.coords);
+      document.body.appendChild(locOut);
+
     },
     function(error) {
       console.log(error);
+      cvs.innerHTML = "Error obtaining GPS location from network: " + error;
     }, {enableHighAccuracy: true});
 }
+else {
+  cvs.innerHTML = "GAH! Geolocation is not supported on this browser. :(";
+}
 
-//map = new google.maps.Map(cvs, opts);
+
+function convertToDMS(coords) {
+  var out = '',
+      lat = coords.latitude,
+      lng = coords.longitude,
+      degLat = 0,
+      degLng = 0,
+      minLat = 0,
+      minLng = 0,
+      secLat = 0,
+      secLng = 0,
+      dirLat,
+      dirLng;
+
+  //latitude - N/S (pos/neg)
+  degLat = (lat | 0);
+  minLat = (Math.abs(degLat-lat) * 60) | 0;
+  secLat = Math.round((Math.abs(degLat-lat) * 3600) - (minLat * 60));
+  dirLat = (lat > 0) ? "N" : "S";
+  
+  degLat = Math.abs(degLat);
+  out += degLat + "&deg; " + minLat + "' " + secLat + "\" " + dirLat;
+
+  out += " -- ";
+
+  //longitude E/W (pos/neg)
+  degLng = (lng | 0);
+  minLng = (Math.abs(degLng-lng) * 60) | 0;
+  secLng = Math.round((Math.abs(degLng-lng) * 3600) - (minLng * 60));
+  dirLng = (lng > 0) ? "E" : "W";
+
+  degLng = Math.abs(degLng);
+  out += degLng + "&deg; " + minLng + "' " + secLng + "\" " + dirLng;
+
+  return out;
+}
